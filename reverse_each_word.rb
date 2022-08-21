44c7f020c96f24afad987159a41beb2437cf4258
Joshua <mwalejoshua89@gmail.com>
2022-08-21 10:13:41 +0300

    Coded along successfully

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,12 @@
-# Write your code here
+require 'pry'
+ def reverse_each_word(sentence)
+    words = sentence.split
+    
+    reversed_words = words.map(&:reverse)
+
+    reversed_words.join(" ")
+ end
+
+ binding.pry
+ 0
+
